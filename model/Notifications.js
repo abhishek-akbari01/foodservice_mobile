dfd0e455210b6a467c9a37d248814cf3e97b753a
abhishek-akbari01 <abhishekakbari9668@gmail.com>
2022-04-03 08:32:42 +0530

    first commit

---
+++ model/Notifications.js
@@ -0,0 +1,57 @@
+export default (Notifications = [
+  {
+    id: 1,
+    title: 'Friday offer!!',
+    details: '11AM-2PM 40% off on all Burgers',
+  },
+  {
+    id: 2,
+    title: 'IPL offer!!',
+    details: 'Free Aloo Tikki + Fries + Veg Pizza Puff on Orders Above Rs 349',
+  },
+
+  {
+    id: 3,
+    title: 'Your bengali thali order placed successfully',
+    details: 'Your bengali thali order placed successfully',
+  },
+  {
+    id: 4,
+    title: 'Money added to your wallet',
+    details:
+      '₹ 1,000/- has been added to your wallet successfully and remaining balance is ₹ 1,150/-',
+  },
+  {
+    id: 5,
+    title: 'Add money to your wallet',
+    details:
+      'Only ₹ 150/- is left in your wallet. Add some more amount to place your order quickly.',
+  },
+  {
+    id: 6,
+    title: 'Your bengali thali order placed successfully',
+    details: 'Your bengali thali order placed successfully',
+  },
+  {
+    id: 7,
+    title: 'Money added to your wallet',
+    details:
+      '₹ 1,000/- has been added to your wallet successfully and remaining balance is ₹ 1,150/-',
+  },
+  {
+    id: 8,
+    title: 'Add money to your wallet',
+    details:
+      'Only ₹ 150/- is left in your wallet. Add some more amount to place your order quickly.',
+  },
+  {
+    id: 9,
+    title: 'Your pizza order placed successfully',
+    details: 'Your pizza order placed successfully',
+  },
+  {
+    id: 10,
+    title: 'Your bengali thali order is ready',
+    details: 'Your bengali thali order is ready',
+  },
+]);
